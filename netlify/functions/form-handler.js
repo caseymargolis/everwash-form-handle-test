af784d4128b0c9d75fe39b02f86fe4c29b76497a
caseymargolis <38667116+caseymargolis@users.noreply.github.com>
2025-05-09 04:06:47 +0600

    Update form-handler.js

--- netlify/functions/form-handler.js
+++ netlify/functions/form-handler.js
@@ -14,6 +14,7 @@ exports.handler = async (event) => {
         return {
             statusCode: 200,
             body: JSON.stringify({
+                status: 'success',
                 message: "Form data received successfully!",
                 data: formData,
             }),
